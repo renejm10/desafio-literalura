cd32277e276cf3d69445988a22c7cda9a4301e68
Daniel Jimenez <rene.jm10@gmail.com>
2024-05-31 17:15:54 -0600

    se elimino archivos para restructurar codigo desde cero

--- src/main/java/com/renejm/LiterAlura/LiterAluraApplication.java
+++ src/main/java/com/renejm/LiterAlura/LiterAluraApplication.java
@@ -1,13 +1,21 @@
 package com.renejm.LiterAlura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiterAluraApplication {
+public class LiterAluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception{
+
+	}
+
 }
+
+
--- src/main/java/com/renejm/LiterAlura/service/IConvierteDatos.java
+++
@@ -1,5 +0,0 @@
-package com.renejm.LiterAlura.service;
-
-public interface IConvierteDatos {
-    <T> T obtenerDatos(String json, Class<T> clase);
-}
